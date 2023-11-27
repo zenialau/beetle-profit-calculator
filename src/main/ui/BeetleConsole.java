8a979210580d23bfd2e08c4e64d7b19f65d577e9
Zenia Lau <zenialaulau.lau@gmail.com>
2023-11-26 20:13:05 -0800

    console: implement buyers-related actions

--- src/main/ui/BeetleConsole.java
+++ src/main/ui/BeetleConsole.java
@@ -1,166 +1,244 @@
-//package src.main.ui;
-//
-//import src.main.model.ProfitCalculator;
-//import src.main.model.company.Buyer;
-//import src.main.model.company.BuyersMap;
-//import src.main.model.company.SuppliersMap;
-//import src.main.model.company.Trader;
-//import src.main.model.exception.DuplicateTraderException;
-//import src.main.model.inventory.PurchaseList;
-//
-//import java.util.Map;
-//import java.util.Scanner;
-//
-//public class BeetleConsole {
-//    private Scanner input;
-//    private BuyersMap buyers;
-//    private SuppliersMap suppliers;
-//    private ProfitCalculator calc;
-//
-//    public BeetleConsole() {
-//        runConsole();
-//    }
-//
-//    private void runConsole() {
-//        init();
-//        mainMenu();
-//    }
-//
-//    private void mainMenu() {
-//        boolean keepGoing = true;
-//        String command;
-//
-//        while (keepGoing){
-//            displayMainMenu();
-//            command = input.next();
-//            command = command.toLowerCase();
-//
-//            if (command.equals("q")) {
-//                keepGoing = false;
-//            } else {
-//                processCommand(command);
-//            }
-//        }
-//    }
-//
-//    private void displayMainMenu() {
-//        System.out.println("Buyers > b");
-//        System.out.println("Suppliers > s");
-//        System.out.println("See profit > p");
-//        System.out.println("Quit > q");
-//    }
-//
-//    private void processCommand(String command) {
-//        switch (command) {
-//            case "b":
-//                buyersMenu();
-//                break;
-//            case "s":
-//                suppliersMenu();
-//                break;
-//            case "p":
-//                calculatorMenu();
-//                break;
-//        }
-//    }
-//
-//    private void buyersMenu() {
-//        boolean keepGoing = true;
-//        String command;
-//
-//        while (keepGoing) {
-//            displayBuyersMenu();
-//            command = input.next();
-//            command = command.toLowerCase();
-//
-//            if (command.equals("q")) {
-//                keepGoing = false;
-//            } else {
-//                processBuyersCommand(command);
-//            }
-//        }
-//    }
-//
-//    private void displayBuyersMenu() {
-//        Map<Trader, PurchaseList> map = buyers.getTradersAndPurchases();
-//        for (Trader buyer : map.keySet()) {
-//            System.out.println(buyer.getName());
-//        }
-//        System.out.println();
-//        System.out.println("Add buyer > a");
-//        System.out.println("Add purchase > enter buyer's name");
-//        System.out.println("Quit > q");
-//    }
-//
-//    // REQUIRES: no buyers have the same name
-//    private void processBuyersCommand(String command) {
-//        if (command.equals("a")) {
-//            addBuyerMenu();
-//        }
-//        for (Trader buyer : buyers.getTradersAndPurchases().keySet()) { //!!!
-//            if (buyer.getName().toLowerCase().equals(command)) {
-//                addBuyerPurchaseMenu(buyer);
-//            }
-//        }
-//    }
-//
-//    private void addBuyerMenu() {
-//        System.out.println("Name: ");
-//        String name = input.next();
-//        System.out.println("IG account: ");
-//        String igAccount = input.next();
-//        System.out.println("Address: ");
-//        String address = input.next();
-//        Buyer buyer = new Buyer(name, igAccount, address);
-//        try {
-//            buyers.addTrader(buyer);
-//        } catch (DuplicateTraderException e) {
-//            System.out.println("Buyer already exist!");
-//        }
-//    }
-//
-//    private void addBuyerPurchaseMenu(Trader buyer) { // Trader type? !!!
-//        //stub
-//        //!!!
-//        // how specific does he want it???
-//        // - do you have to already bought the product from suppliers
-//        //   or
-//        // - can add whatever you want
-//        //   if this is the case, how???
-//    }
-//
-//    private void suppliersMenu() {
-//
-//    }
-//
-//    private void calculatorMenu() {
-//        boolean keepGoing = true;
-//        String command;
-//
-//        while (keepGoing) {
-//            displayCalculatorMenu();
-//            command = input.next();
-//            command = command.toLowerCase();
-//
-//            if (command.equals("q")) {
-//                keepGoing = false;
-//            }
-//        }
-//    }
-//
-//    private void displayCalculatorMenu() {
-//        System.out.println("Profit Calculator");
-//        calc.update();
-//        System.out.println("Profit: " + calc.getProfit());
-//        System.out.println("Reload > r");
-//        System.out.println("Quit > q");
-//    }
-//
-//    private void init() {
-//        input = new Scanner(System.in);
-//        buyers = new BuyersMap();
-//        suppliers = new SuppliersMap();
-//        calc = new ProfitCalculator(buyers, suppliers);
-//    }
-//
-//}
+package src.main.ui;
+
+import src.main.model.ProfitCalculator;
+import src.main.model.company.Buyer;
+import src.main.model.company.BuyersMap;
+import src.main.model.company.SuppliersMap;
+import src.main.model.company.Trader;
+import src.main.model.exception.DuplicateException;
+import src.main.model.inventory.InventoryItem;
+import src.main.model.inventory.Purchase;
+import src.main.model.inventory.PurchaseList;
+
+import java.util.Scanner;
+
+public class BeetleConsole {
+    private Scanner input;
+    private BuyersMap buyers;
+    private SuppliersMap suppliers;
+    private ProfitCalculator calc;
+
+    public BeetleConsole() {
+        runConsole();
+    }
+
+    private void runConsole() {
+        init();
+        mainMenu();
+    }
+
+    private void mainMenu() {
+        boolean keepGoing = true;
+        String command;
+
+        while (keepGoing){
+            displayMainMenu();
+            command = input.next();
+            command = command.toLowerCase();
+
+            if (command.equals("q")) {
+                keepGoing = false;
+            } else {
+                processCommand(command);
+            }
+        }
+    }
+
+    private void displayMainMenu() {
+        System.out.println("Buyers > b");
+        System.out.println("Suppliers > s");
+        System.out.println("See profit > p");
+        System.out.println("Quit > q");
+    }
+
+    private void processCommand(String command) {
+        switch (command) {
+            case "b":
+                buyersMenu();
+                break;
+            case "s":
+                suppliersMenu();
+                break;
+            case "p":
+                calculatorMenu();
+                break;
+        }
+    }
+
+    private void buyersMenu() {
+        boolean keepGoing = true;
+        String command;
+
+        while (keepGoing) {
+            displayBuyersMenu();
+            command = input.next();
+            command = command.toLowerCase();
+
+            if (command.equals("q")) {
+                keepGoing = false;
+            } else {
+                processBuyersCommand(command);
+            }
+        }
+    }
+
+    private void displayBuyersMenu() {
+        for (Trader buyer : buyers.getListOfBuyers()) {
+            System.out.println(buyer.getName());
+        }
+        System.out.println();
+        System.out.println("Add buyer > a");
+        System.out.println("View buyer details or add purchase > enter buyer's name");
+        System.out.println("Quit > q");
+    }
+
+    // REQUIRES: no buyers have the same name
+    private void processBuyersCommand(String command) {
+        if (command.equals("a")) {
+            addBuyerMenu();
+        }
+        for (Buyer buyer : buyers.getListOfBuyers()) {
+            if (buyer.getName().toLowerCase().equals(command)) {
+                viewBuyerMenu(buyer);
+            }
+        }
+    }
+
+    private void addBuyerMenu() {
+        System.out.println("Name: ");
+        String name = input.next();
+        System.out.println("IG account: ");
+        String igAccount = input.next();
+        System.out.println("Address: ");
+        String address = input.next();
+        Buyer buyer = new Buyer(name, igAccount, address);
+        try {
+            buyers.addBuyer(buyer);
+        } catch (DuplicateException e) {
+            System.out.println("Buyer already exist!");
+        }
+    }
+
+    private void viewBuyerMenu(Buyer buyer) {
+        boolean keepGoing = true;
+        String command;
+
+        while (keepGoing) {
+            displayViewBuyerMenu(buyer);
+            command = input.next();
+            command = command.toLowerCase();
+
+            if (command.equals("q")) {
+                keepGoing = false;
+            } else {
+                processViewBuyerCommand(command, buyer);
+            }
+        }
+    }
+
+    private void displayViewBuyerMenu(Buyer buyer) {
+        for (Purchase purchase : buyers.getPurchaseList(buyer).getPurchaseList()) {
+            for (InventoryItem item : purchase.getItems()) {
+                System.out.print(item.getName() + ": " + item.getPrice() + ", ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        System.out.println("Add purchase to " + buyer.getName() + "(buyer) > a");
+        System.out.println("Quit > q");
+    }
+
+    private void processViewBuyerCommand(String command, Buyer buyer) {
+        if (command.equals("a")) {
+            addPurchaseMenu(buyer);
+        }
+    }
+
+    private void addPurchaseMenu(Buyer buyer) {
+        Purchase purchase = new Purchase();
+        boolean keepGoing = true;
+        String command;
+
+        while (keepGoing) {
+            displayAddPurchaseMenu(purchase);
+            command = input.next();
+            command = command.toLowerCase();
+
+            if (command.equals("q")) {
+                keepGoing = false;
+            }  else if (command.equals("c")) {
+                keepGoing = false;
+                buyers.getPurchaseList(buyer).addPurchase(purchase);
+            } else {
+                processAddPurchaseCommand(command, purchase);
+            }
+        }
+    }
+
+    private void displayAddPurchaseMenu(Purchase purchase) {
+        for (InventoryItem item : purchase.getItems()) {
+            System.out.println(item.getName() + ": " + item.getPrice());
+        }
+        System.out.println("Add item to purchase > a");
+        System.out.println("Confirm purchase > c");
+        System.out.println("Quit > q");
+    }
+
+    private void processAddPurchaseCommand(String command, Purchase purchase) {
+        if (command.equals("a")) {
+            addItemMenu(purchase);
+        }
+    }
+
+    private void addItemMenu(Purchase purchase) {
+        System.out.println("Name: ");
+        String name = input.next();
+        System.out.println("Description: ");
+        String description = input.next();
+        System.out.println("Size (mm): ");
+        double size = Double.parseDouble(input.next());
+        System.out.println("Quality: ");
+        String quality = input.next();
+        System.out.println("Comment: ");
+        String comment = input.next();
+        System.out.println("Price (CAD): ");
+        double price = Double.parseDouble(input.next());
+        InventoryItem item = new InventoryItem(name, description, size, quality, comment, price);
+        purchase.addItem(item);
+    }
+
+    private void suppliersMenu() {
+
+    }
+
+    private void calculatorMenu() {
+        boolean keepGoing = true;
+        String command;
+
+        while (keepGoing) {
+            displayCalculatorMenu();
+            command = input.next();
+            command = command.toLowerCase();
+
+            if (command.equals("q")) {
+                keepGoing = false;
+            }
+        }
+    }
+
+    private void displayCalculatorMenu() {
+        System.out.println("Profit Calculator");
+        calc.update();
+        System.out.println("Profit: " + calc.getProfit());
+        System.out.println("Reload > r");
+        System.out.println("Quit > q");
+    }
+
+    private void init() {
+        input = new Scanner(System.in);
+        buyers = new BuyersMap();
+        suppliers = new SuppliersMap();
+        calc = new ProfitCalculator(buyers, suppliers);
+    }
+
+}
--- src/test/model/ProfitCalculatorTest.java
+++ src/test/model/ProfitCalculatorTest.java
@@ -1,57 +1,57 @@
-//package src.test.model;
-//
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import src.main.model.ProfitCalculator;
-//import src.main.model.company.BuyersMap;
-//import src.main.model.company.SuppliersMap;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//public class ProfitCalculatorTest extends ModelTest{
-//    ProfitCalculator testCalculator;
-//    BuyersMap buyers;
-//    SuppliersMap suppliers;
-//
-//    @BeforeEach
-//    void setUp() {
-//        testPurchase.addItem(testItem1);
-//        testPurchase.addItem(testItem2);
-//
-//        buyers = new BuyersMap();
-//        try {
-//            buyers.addTrader(testBuyer);
-//        } catch (Exception e) {
-//            fail("Exception not expected.");
-//        }
-//        buyers.addPurchase(testBuyer, testPurchase);
-//
-//        suppliers = new SuppliersMap();
-//        try {
-//            suppliers.addTrader(testSupplier);
-//        } catch (Exception e) {
-//            fail("Exception not expected.");
-//        }
-//        suppliers.addPurchase(testSupplier, testPurchase);
-//
-//        testCalculator = new ProfitCalculator(buyers, suppliers);
-//    }
-//
-//    @Test
-//    void testConstructor() {
-//        assertEquals(0, testCalculator.getRevenue());
-//        assertEquals(0, testCalculator.getExpense());
-//    }
-//
-//    @Test
-//    void testGetProfit0() {
-//        testCalculator.update();
-//        assertEquals(0, testCalculator.getProfit());
-//    }
-//    @Test
-//    void testGetProfitGeneral() {
-//        buyers.addPurchase(testBuyer, testPurchase);
-//        testCalculator.update();
-//        assertEquals(250, testCalculator.getProfit());
-//    }
-//}
+package src.test.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import src.main.model.ProfitCalculator;
+import src.main.model.company.BuyersMap;
+import src.main.model.company.SuppliersMap;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ProfitCalculatorTest extends ModelTest{
+    ProfitCalculator testCalculator;
+    BuyersMap buyers;
+    SuppliersMap suppliers;
+
+    @BeforeEach
+    void setUp() {
+        testPurchase1.addItem(i1);
+        testPurchase1.addItem(i2);
+
+        buyers = new BuyersMap();
+        try {
+            buyers.addBuyer(testBuyer1);
+        } catch (Exception e) {
+            fail("Exception not expected.");
+        }
+        buyers.addPurchase(testBuyer1, testPurchase1);
+
+        suppliers = new SuppliersMap();
+        try {
+            suppliers.addSupplier(testSupplier1);
+        } catch (Exception e) {
+            fail("Exception not expected.");
+        }
+        suppliers.addPurchase(testSupplier1, testPurchase1);
+
+        testCalculator = new ProfitCalculator(buyers, suppliers);
+    }
+
+    @Test
+    void testConstructor() {
+        assertEquals(0, testCalculator.getRevenue());
+        assertEquals(0, testCalculator.getExpense());
+    }
+
+    @Test
+    void testGetProfit0() {
+        testCalculator.update();
+        assertEquals(0, testCalculator.getProfit());
+    }
+    @Test
+    void testGetProfitGeneral() {
+        buyers.addPurchase(testBuyer1, testPurchase1);
+        testCalculator.update();
+        assertEquals(60, testCalculator.getProfit());
+    }
+}
